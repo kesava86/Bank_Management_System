f532f6802f522aaab51e5a2d7c05b08a3d0d7f0d
kesava86 <kkesava493@gmail.com>
2025-12-18 21:49:53 +0530

    Connenction

--- src/bank/management/system/Mini.java
+++ src/bank/management/system/Mini.java
@@ -40,7 +40,7 @@ public class Mini extends JFrame implements ActionListener {
         add(label4);
 
         try {
-            Con c = new Con();
+            Conn c = new Conn();
             ResultSet resultSet = c.statement.executeQuery("select * from login where pin = '"+pin+"'");
             while (resultSet.next()){
                 label3.setText("Card Number : "+resultSet.getString("card_number").substring(0,4)+"XXXXXXXX"+resultSet.getString("card_number").substring(12));
